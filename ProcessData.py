3f4a752e78e764155c4fb0662ab7fa307e1ef72d
jlpusher <jlpusher@unomaha.edu>
2025-05-11 05:56:56 +0000

    all but major year

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jessica Pusher
+#Date: 5.11.25
+#Assignment: Lab 8
 
 import random
 
@@ -11,13 +11,32 @@ def main():
   inFile = open("names.dat", 'r')
   outFile = open("StudentList.csv", 'w')
 
-  #Process each line of the input file and output to the CSV file
+  outFile.write("Last Name" + "," + "First Name" + "," + "UserID" + "," + "Major-Year" + "\n")
 
+  #Process each line of the input file and output to the CSV file
+  #line = inFile.readline()
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    idNum = data[3]
+    student_id = makeID(first, last, idNum)
+    output = last + "," + first + "," + student_id + "\n"
+    outFile.write(output)
+    #print(student_id)
 
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+
+def makeID(first, last, idNum):
+  #print(first, last, idNum)
+  while len(last) < 5:
+    last = last + "x"
+  id = first[0] + last + idNum[-3:]
+  
+  return id
 
 if __name__ == '__main__':
   main()
